0d054c0f33e72e3163a24357fc6420e8a3d7f08e
Andres Nguyen <thuongnguyen.it78@gmail.com>
2022-06-17 18:56:39 +0700

    add some feature

---
+++ src/features/post/post.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+import {
+  postActive, postInactive
+} from '@/constants/post.constant'
+import slug from 'mongoose-slug-generator'
+
+const options = {
+  separator: '-',
+  lang: 'en',
+  truncate: 120,
+}
+
+mongoose.plugin(slug, options)
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'user',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    slug: { type: String, slug: 'title', slug_padding_size: 3, unique: true },
+    status: {
+      type: Number,
+      default: postInactive,
+      enum: [postInactive, postActive],
+    },
+    tagList: [{ type: mongoose.Types.ObjectId, ref: 'tag', default: [] }],
+    likeList: [{ type: mongoose.Types.ObjectId, ref: 'user', default: [] }],
+    saverList: [{ type: mongoose.Types.ObjectId, ref: 'user', default: [] }],
+    delete: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('post', postSchema, 'post')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,27 @@
-import express from 'express';
-const router = express.Router();
+import express from 'express'
+const router = express.Router()
 
 import authRoute from '@/features/auth/auth.route'
 import uploadRoute from '@/features/upload/upload.route'
 import userRoute from '@/features/user/user.route'
+import tagRoute from '@/features/tag/tag.route'
+import postRoute from '@/features/post/post.route'
+import commentRoute from '@/features/comment/comment.route'
+import songRoute from '@/features/song/song.route'
+import playlistRoute from '@/features/playlist/playlist.route'
 
 
 function route(app) {
-  router.use('/v1/auth', authRoute)
-  router.use('/v1/upload', uploadRoute)
-  router.use('/v1/users', userRoute)
+  router.use('/auth', authRoute)
+  router.use('/upload', uploadRoute)
+  router.use('/users', userRoute)
+  router.use('/tags', tagRoute)
+  router.use('/posts', postRoute)
+  router.use('/comments', commentRoute)
+  router.use('/songs', songRoute)
+  router.use('/playlist', playlistRoute)
 
-  app.use(router)
+  app.use('/v1', router)
 }
 
 export default route
